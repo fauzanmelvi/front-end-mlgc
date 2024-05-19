1ac64380a8d1765c186d86bfb2e726bff9fc64d2
fauzan <fauzan.melvi.h@gmail.com>
2024-05-19 19:44:06 +0700

    [first]

---
+++ src/scripts/api.js
@@ -0,0 +1,19 @@
+// TODO: Silakan sesuaikan BASE URL dari endpoint Anda
+const BASE_URL = '34.34.222.123:3000';
+
+const ENDPOINT = {
+  predict: `${BASE_URL}/predict`,
+};
+
+class PredictAPI {
+  static async predict(data) {
+    const response = await fetch(ENDPOINT.predict, {
+      method: 'POST',
+      body: data,
+      redirect: 'follow',
+    });
+
+    const json = await response.json();
+    return json;
+  }
+}
